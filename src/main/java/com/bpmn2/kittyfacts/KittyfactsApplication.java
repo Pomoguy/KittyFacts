cd1cdb01f8c4cb2c6b4860c4ca531b07e52d11a7
Grisha <pomogaibo.g.s@yandex.ru>
2022-03-17 09:14:11 +0300

    camunda initialized

--- src/main/java/com/bpmn2/kittyfacts/KittyfactsApplication.java
+++ src/main/java/com/bpmn2/kittyfacts/KittyfactsApplication.java
@@ -1,9 +1,11 @@
 package com.bpmn2.kittyfacts;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class KittyfactsApplication {
 
 	public static void main(String[] args) {
