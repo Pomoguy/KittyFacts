9ee1ddd395e4245ee8c75b66b0ae31b8ea217cd5
Grisha <pomogaibo.g.s@yandex.ru>
2022-03-17 08:30:59 +0300

    init
    init

---
+++ src/main/java/com/bpmn2/kittyfacts/KittyfactsApplication.java
@@ -0,0 +1,13 @@
+package com.bpmn2.kittyfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittyfactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittyfactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpmn2/kittyfacts/KittyfactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpmn2.kittyfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittyfactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
