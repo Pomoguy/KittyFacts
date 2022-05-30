eeca41b87261188748de500aff696e221ea36f61
Grisha <pomogaibo.g.s@yandex.ru>
2022-05-30 08:25:03 +0300

    Исправления после ревью
    + пушка тесты

--- kittyfacts-core/src/main/java/com/bpmn2/kittyfacts/delegate/GetPictureDelegate.java
+++ kittyfacts-core/src/main/java/com/bpmn2/kittyfacts/delegate/GetPictureDelegate.java
@@ -1,34 +1,41 @@
 package com.bpmn2.kittyfacts.delegate;
 
 
-import com.bpmn2.kittyfacts.model.KittyFact;
+
 import com.bpmn2.kittyfacts.service.CataasClient;
+import org.apache.commons.io.FileUtils;
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.camunda.bpm.engine.variable.value.ObjectValue;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 
 @Component("getPictureDelegate")
 public class GetPictureDelegate implements JavaDelegate {
 
+    private final static Logger LOGGER = Logger.getLogger(GetPictureDelegate.class.getName());
+
     @Autowired
     CataasClient cataasClient;
 
-    @Override
-    public void execute(DelegateExecution delegateExecution) {
+    @Autowired
+    private ResourceLoader resourceLoader;
 
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws IOException {
         try {
-            ObjectValue objectValue = delegateExecution.getVariableTyped("kittyFact");
-            KittyFact kittyFact = objectValue.getValue(KittyFact.class);
-            kittyFact.setPicture(cataasClient.getCataas());
-            delegateExecution.setVariable("kittyFact", kittyFact);
+            delegateExecution.setVariable("picture", cataasClient.getCataas());
         } catch (Exception exception) {
+
+            File inputFile = resourceLoader.getResource("classpath:/static/sry.jpg").getFile();
+            delegateExecution.setVariable("picture",FileUtils.readFileToByteArray(inputFile));
             throw new BpmnError("badContent", exception.getMessage());
         }
     }
--- kittyfacts-core/src/main/java/com/bpmn2/kittyfacts/service/CataasClient.java
+++ kittyfacts-core/src/main/java/com/bpmn2/kittyfacts/service/CataasClient.java
@@ -3,6 +3,7 @@ package com.bpmn2.kittyfacts.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import javax.ws.rs.Consumes;
@@ -20,7 +21,6 @@ public class CataasClient {
 
     @Consumes("image/jpeg")
     public byte[] getCataas() throws IOException {
-
         restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());
         byte[] imageBytes = restTemplate.getForObject(url + "/cat", byte[].class);
         return imageBytes;
